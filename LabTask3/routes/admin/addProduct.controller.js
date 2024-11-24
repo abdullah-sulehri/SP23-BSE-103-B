142abbc79e04553d2c0e001ee4a81dfb0f4b32c0
Abdullah Sulehri <abdullah.sulehri.11@gmail.com>
2024-11-24 20:57:34 +0500

    ..

--- LabTask3/routes/admin/addProduct.controller.js
+++ LabTask3/routes/admin/addProduct.controller.js
@@ -1,18 +1,15 @@
 const express = require("express");
 let router = express.Router();
 
-router.post("/admin/createproduct", (req, res) => {
-    try {    
+router.get("/admin/create", (req, res) => {
+     
     
-    return   res.render("admin/createproduct",{
+    res.render("admin/createproduct",{
    
          pageTitle : "add prodcuts",  
         })
-    }
-    catch (err) {
-        console.error("Error fetching products:", err);
-        res.status(500).send("Error fetching products from the database.");
-      }
+ 
+    
 
 })
 
